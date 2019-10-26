a620181c9e9ac13154b4773ef2ab4dad4428ae4c
Evan Schweitzer <eschwei2@masonlive.gmu.edu>
2019-10-26 16:30:19 -0500

    a tag for scroll

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -3,7 +3,8 @@ import {uniqueId} from '../Events/Events'
 
 const Product = (props) => {
     return (
-    <div href="#search-bar-root" className="Carousel-Product" onClick={
+    <a href="#search-bar-root" >
+        <div  className="Carousel-Product" onClick={
         (event) => {
             window.uniqueId = props.product_id
             window.dispatchEvent(uniqueId);
@@ -14,6 +15,7 @@ const Product = (props) => {
         <p className="Carousel-Price">{props.price}</p>
         <p className="Carousel-Shipping">{props.shipping === true? "Free Shipping" : "Not Free Shipping"} </p>
     </div>
+    </a>
     )
 }
 export default Product  
